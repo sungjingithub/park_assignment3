6cfccc5ca7dce695246304cf0344434d484ae0d5
sungjingithub <60050629+sungjingithub@users.noreply.github.com>
2020-02-23 20:33:57 -0500

    Update CAI4.java

--- Park_Problem1/CAI4.java
+++ Park_Problem1/CAI4.java
@@ -81,7 +81,7 @@ public class CAI4 {
 	}
 	static boolean displayCompletionMessage(float score) {
 		String tryAnswer;
-		System.out.println("Your total score was " + score*100 + "%\n");
+		System.out.println("Your total score was " + (int)(score*100) + "%\n");
 		
 		if(score < 0.75f) {
 			System.out.println("Please ask your teacher for extra help.");
